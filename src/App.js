2fce76dbc2e7837c16b99ec1055781e592520875
Jason Pembleton <pembo.dev@gmail.com>
2022-01-31 18:05:45 +0800

    add boilerplate html tags

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import './App.css';
 function App() {
   return (
     <div>
-      
+      <header>
+        <div className="header">Header goes here</div>
+      </header>
+      <main>
+        <div className="main-content">
+          <div className="filter">Filter </div>
+          <div className="content">Content</div>
+        </div>
+
+        <aside>Subreddit</aside> 
+
+      </main>
     </div>
   );
 }
