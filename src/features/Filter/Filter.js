b2af3b166ed3c5d43248609c174b41f5cf909e7e
Jason Pembleton <pembo.dev@gmail.com>
2022-02-01 15:15:31 +0800

    add buttons and text for filter

--- src/features/Filter/Filter.js
+++ src/features/Filter/Filter.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import {
+    AiFillPlusSquare
+} from 'react-icons/ai';
+import { AiOutlineSearch } from 'react-icons/ai';
 import './Filter.css';
 
 const Filter = () => {
     return (
-        <div>
-        </div>
+        <div className="filter">
+            <div className="icon"> 
+                <button><AiFillPlusSquare /></button>
+                <p>Best</p>
+            </div>
+            <div className="icon"> 
+                <button><AiFillPlusSquare /></button>
+                <p>Hot</p>
+            </div>
+            <div className="icon"> 
+                <button><AiFillPlusSquare /></button>
+                <p>New</p>
+            </div>
+            <div className="icon"> 
+                <button><AiFillPlusSquare /></button>
+                <p>Top</p>
+            </div>
+            <div className="search">
+                <button> <AiOutlineSearch /></button>
+                <input type="text" placeholder="Search"></input>
+            </div>
 
+        </div>
     );
 };
 
